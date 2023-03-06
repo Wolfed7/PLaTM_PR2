56649ea75ba12bf04861e0332d6f81a3bc13d040
Wolfed7 <90997660+Wolfed7@users.noreply.github.com>
2023-03-06 19:15:31 +0700

    Добавьте файлы проекта.

---
+++ PLaTM_PR2/PLaTM_PR2.cpp
@@ -0,0 +1,47 @@
+﻿#include "Tables.h"
+
+using namespace std;
+
+const string Keywords = "Keywords.txt";
+const string Operators = "Operators.txt";
+const string Separators = "Separators.txt";
+const string Brackets = "Brackets.txt";
+const string Specials = "Specials.txt";
+
+int main()
+{
+   Tables tables
+   (
+      Keywords,
+      Operators,
+      Separators,
+      Brackets,
+      Specials
+   );
+
+   // Проверка поиска в постоянных таблицах.
+   tables.SearchOnStatic("int");
+
+   // Добавление в переменные таблицы.
+   pair<int, int> a = tables.IncludeInDynamic("counter", Int, false);
+   pair<int, int> b = tables.IncludeInDynamic("counter", Int, false);
+   tables.IncludeInDynamic("132", Int);
+   tables.IncludeInDynamic("siztable", Int, false);
+   tables.IncludeInDynamic("IsProgram", Int, false);
+   tables.IncludeInDynamic("table213", Int, false);
+   tables.IncludeInDynamic("421", Int);
+
+   // Возвращение адреса идентификатора.
+   pair<int, int> SearchResult = tables.SearchOnDynamic("siztable");
+
+   // Возвращение структуры, содержащей атрибуты.
+   Variable *identifier = tables.SearchOnDynamic(SearchResult);
+
+   // Изменение атрибута Type.
+   tables.SetType(SearchResult, Void);
+
+   // Атрибут изменился.
+   identifier = tables.SearchOnDynamic(SearchResult);
+
+   return 0;
+}
---
+++ PLaTM_PR2/StaticTable.h
@@ -0,0 +1,25 @@
+#pragma once
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+class StaticTable
+{
+private:
+
+   vector<string> Table;
+
+public:
+
+   // В качестве аргумента принимает файл с постоянной таблицей.
+   StaticTable(string FilePath);
+
+   // Возвращает номер найденного в таблице элемента,
+   // в случае неудачи возвращает -1. 
+   int BinarySearch(string Name);
+
+   // Возвращает размер таблицы.
+   size_t Lenth();
+};
+
